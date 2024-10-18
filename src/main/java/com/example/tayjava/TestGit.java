9a22858b2a79a88891663e34229403695e1e8a48
Đức <ducdam2209@gmail.com>
2024-10-17 22:00:15 -0700

    second commit

--- src/main/java/com/example/tayjava/TestGit.java
+++ src/main/java/com/example/tayjava/TestGit.java
@@ -3,6 +3,6 @@ package com.example.tayjava;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello World");
     }
 }
